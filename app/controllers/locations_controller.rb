66b2852dcd79dc0d2fe265ec1e70d8d201f89690
Klajd Veizaj <klajdv@gmail.com>
2021-02-23 20:03:58 +0100

    initial commit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,26 +1,32 @@
 class LocationsController < ApplicationController
 
-before_action  :find_location, only:[:show, :edit,:update,:destroy]
+before_action  :find_location, only:[:show, :edit, :update, :destroy]
 
 def find_location
     @location = Location.find(params[:id])
-
 end
 
 
 def index
 	@location = Location.all.order("created_at DESC")
-
 end
+
+def filter
+	start_at = params[:start_at]
+	end_at = params[:end_at]
+	start_km = params[:start_km]
+	end_km = params[:end_km]
+    @location = Location.where("date between ? and ? ",start_at,end_at)
+	
+end
+
 
 def new	
 	@location = Location.new
-
 end
 
 def show
     @location = Location.find(params[:id])
-
 end
 
 def create
@@ -44,25 +50,6 @@ def destroy
 	@location.destroy
 	redirect_to root_path
 end
-
-def filter_location
-	@start_at = params[:start_at]
-	@end_at = params[:end_at]
-	@start_km = params[:start_km]
-	@end_km = params[:end_km]
-    @date = Location.find(params[:date])
-    @location = Location.where("date=? between start_at=? and end_at=? ",date,start_at,end_at)
-
-
-render :index
-
-
-end
-
-def sum(start_km)
-  start_km.inject(0){|sum, el| sum + el}
-end
-
 
 
 def location_params
